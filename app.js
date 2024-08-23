59867f2476245ed541e507b66671faa175da5cd8
usmanramzan342 <usmantt.ramzan@gmail.com>
2024-08-23 18:19:36 +0500

    UsmanTT

---
+++ app.js
@@ -0,0 +1,5 @@
+let table = prompt("please enter any table Number.");
+
+for (let a = 1; a<=10; a++){
+    document.write(table + " " + " * " + " " + a + " " + " = " + " " + (a*table) + "<br / >"  );
+}
